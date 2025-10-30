472af495f6afb624e8fc341435721a4c90cfe76e
ArdalanMolodi <ArdalanMolodi1@gmail.com>
2025-10-30 12:28:05 +0330

    create header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div>
+       <a href="/">
+      <h1 className="text-center font-bold font-serif text-3xl my-5 text-orange-400 hover:text-black group cursor-pointer">
+        Note{" "}
+        <span className="group-hover:text-orange-400 text-black">Book</span> Web
+      </h1>
+    </a>
+    </div>
+   
+  );
+}
