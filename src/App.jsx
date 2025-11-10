f14de818e2162cde4ddc2327d6df254834556d60
ArdalanMolodi <ArdalanMolodi1@gmail.com>
2025-11-10 11:40:27 +0330

    create destracture of web

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,5 @@
-import { useState } from "react";
-
 export default function App() {
-  const [counter, setcounter] = useState(0);
   return (
-    <div>
-      <a href="/">
-        <h1 className="text-center font-bold font-serif text-3xl my-5 text-orange-400 hover:text-black group cursor-pointer">
-          Note{" "}
-          <span className="group-hover:text-orange-400 text-black">Book</span>{" "}
-          Web
-        </h1>
-      </a>
-      <div className="flex justify-evenly items-center">
-        <h2 className="font-bold text-2xl">
-          My Notes: <span className="text-orange-400">{counter}</span>
-        </h2>
-
-        <select
-          id="selection"
-          name=""
-          className="border-gray-500 text-orange-400 border rounded-sm px-2 flex  font-bold">
-          <option value="" className="">
-            sort based on least
-          </option>
-          <option value="" className="">
-            sort by last date
-          </option>
-          <option value="" className="">
-            sort by first
-          </option>
-        </select>
-      </div>
-    </div>
+   <div>Hello World</div>
   );
 }
